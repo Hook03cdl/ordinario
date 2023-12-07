1e668361e02a9b09e6ebab7bdb6ae20b6ba81062
Hook03cdl <carlosduverney@gmail.com>
2023-12-06 22:13:34 -0700

    correccion del codigo y creacion de las funciones restantes

--- script.js
+++ script.js
@@ -1,54 +1,115 @@
 var contenain = document.querySelector('.contenain-canvas');
 var canvas = document.getElementById('my-canvas');
-var options = document.querySelectorAll('li');
-var color = '';
+var options = document.querySelectorAll('.option');
+var colors = document.querySelectorAll('.selectColor');
+var action = document.querySelector('.active').id;
+var fill = document.getElementById('fill');
+var size = document.getElementById('size');
 
 canvas.width = contenain.offsetWidth;
 canvas.height = contenain.offsetHeight;
+
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = '#fff';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+var isDrawing = false;
+var brushwidth = 5;
+var x, y, snapShot;
+var colorSelect;
 
 options.forEach((option) => {
 	option.addEventListener('click', () => {
 		if ((r = document.querySelector('.active'))) r.classList.remove('active');
 		option.classList.add('active');
-		console.log(option.getAttribute('data-type'));
+		action = option.id;
+		console.log(action);
 	});
 });
 
-var ctx = canvas.getContext('2d');
-
-ctx.fillStyle = '#fff';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-var isDrawing = false;
-var x = 0;
-var y = 0;
-
-canvas.addEventListener('mousedown', (e) => {
-	color = document.querySelector('input:checked').value;
-
+var drawStart = (e) => {
 	isDrawing = true;
 	x = e.offsetX;
 	y = e.offsetY;
+	ctx.beginPath();
+	ctx.lineWidth = brushwidth;
+	ctx.strokeStyle = colorSelect;
+	ctx.fillStyle = colorSelect;
+	snapShot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+};
+
+var rectangle = (e) => {
+	ctx.fillStyle = colorSelect;
+	if (!fill.checked) {
+		return ctx.strokeRect(e.offsetX, e.offsetY, 100, 100);
+	}
+	ctx.fillRect(e.offsetX, e.offsetY, 100, 100);
+};
+
+var circle = () => {
+	ctx.arc(x, y, 50, 0, 2 * Math.PI);
+	fill.checked ? ctx.fill() : ctx.stroke();
+};
+
+const triangle = (e) => {
+	var height = (100 * Math.sqrt(3)) / 2;
+
+	ctx.beginPath();
+	ctx.moveTo(e.offsetX, e.offsetY);
+	ctx.lineTo(e.offsetX - 100 / 2, e.offsetY + height);
+	ctx.lineTo(e.offsetX + 100 / 2, e.offsetY + height);
+	ctx.closePath();
+
+	fill.checked ? ctx.fill() : ctx.stroke();
+};
+
+const draw = (e) => {
+	if (!isDrawing) return;
+	ctx.putImageData(snapShot, 0, 0);
+	switch (action) {
+		case 'brush':
+			ctx.lineTo(e.offsetX, e.offsetY);
+			ctx.stroke();
+			break;
+		case 'eraser':
+			ctx.strokeStyle = '#fff';
+			ctx.lineTo(e.offsetX, e.offsetY);
+			ctx.stroke();
+			break;
+
+		default:
+			break;
+	}
+};
+
+const figure = (e) => {
+	switch (action) {
+		case 'rectangle':
+			rectangle(e);
+			break;
+		case 'circle':
+			circle(e);
+			break;
+		case 'triangle':
+			triangle(e);
+			break;
+
+		default:
+			break;
+	}
+};
+
+size.addEventListener('change', () => {
+	brushwidth = size.value;
+});
+colors.forEach((color) => {
+	color.addEventListener('click', () => {
+		colorSelect = color.value;
+		console.log(colorSelect);
+	});
 });
 
-canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', drawStart);
 canvas.addEventListener('mouseup', () => (isDrawing = false));
-canvas.addEventListener('mouseout', () => (isDrawing = false));
-
-function draw(e) {
-	if (!isDrawing) return;
-	ctx.beginPath();
-	ctx.strokeStyle = color;
-	ctx.moveTo(x, y);
-	ctx.lineTo(e.offsetX, e.offsetY);
-	ctx.stroke();
-	x = e.offsetX;
-	y = e.offsetY;
-}
-
-function update() {
-	if (!isDrawing) return;
-	requestAnimationFrame(update);
-}
-
-update();
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('click', figure);
